fef07f7bc0f796c83725d602d1d585905b6ae49c
Cyril Thomas <cyrilthomas3@gmail.com>
2016-02-23 23:37:47 -0500

    HTML 5 initial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SHOUT IT!</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+    <div id="container">
+        <header>
+            <h1>SHOUT IT! Shoutbox</h1>
+        </header>
+        <div id="shouts">
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
